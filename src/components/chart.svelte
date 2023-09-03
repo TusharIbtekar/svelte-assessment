881002d6a881b1d96599a2eca762ca7a479d1f51
TusharIbtekar <ibtekar8@gmail.com>
2023-09-03 16:42:24 +0600

    feat: integrated polar area chart

---
+++ src/components/chart.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import { browser } from '$app/environment';
+	import { Chart, registerables } from 'chart.js';
+	import { onMount } from 'svelte';
+
+	Chart.register(...registerables);
+
+	let barChartElement: HTMLCanvasElement;
+
+	const data = {
+		labels: ['Red', 'Green', 'Yellow', 'Grey', 'Blue'],
+		datasets: [
+			{
+				label: 'My First Dataset',
+				data: [11, 16, 7, 3, 14],
+				backgroundColor: [
+					'rgb(255, 99, 132)',
+					'rgb(75, 192, 192)',
+					'rgb(255, 205, 86)',
+					'rgb(201, 203, 207)',
+					'rgb(54, 162, 235)'
+				]
+			}
+		]
+	};
+	onMount(() => {
+		if (browser) {
+			new Chart(barChartElement, {
+				type: 'polarArea',
+				data: data,
+				options: {}
+			});
+		}
+	});
+</script>
+
+<div class="col-span-1">
+	<h1>State of JS 2021 Backend Framework Satisfaction</h1>
+	<section>
+		<canvas bind:this={barChartElement} />
+	</section>
+</div>
--- src/routes/chart/+page.svelte
+++ src/routes/chart/+page.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
-	import { countries } from '$lib/countryDataStore';
+	import { countries } from '$lib/stores/country';
 	import Table from '$components/table.svelte';
+	import Chart from '$components/chart.svelte';
 
-	export let data: any;
+	export let data;
 	// console.log(data.countries);
 	countries.set(data.countries);
 </script>
 
-<div>
+<div class="grid grid-cols-3">
 	<Table />
+	<Chart />
 </div>
