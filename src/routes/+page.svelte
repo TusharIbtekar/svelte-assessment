bdfb77e6f526174dc9ef99af6a7b32f8be5d93d2
TusharIbtekar <ibtekar8@gmail.com>
2023-09-04 12:51:16 +0600

    feat: styling landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<div>
-	<a href="/layout">Layout</a>
-	<a href="/chart">Chart</a>
-	<a href="/map">Map</a>
+<div class="flex items-center justify-center h-screen font-medium">
+	<a class="p-5" href="/layout">Layout</a>
+	<a class="p-5" href="/chart">Chart</a>
+	<a class="p-5" href="/map">Map</a>
 </div>
